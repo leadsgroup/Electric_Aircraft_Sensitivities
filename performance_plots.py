6d66dea1ffe45d1b67c92961bdf9485ea7ea5e17
Matthew Clarke <mclarke2@stanford.edu>
2024-09-13 15:29:48 -0500

    Create performance_plots.py

---
+++ performance_plots.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt  
+import numpy as np
+
+def main():
+    
+    L_D_ratio =  25
+    
+    Range =  0
+    
+    return 
+    
+if __name__ == '__main__':
+    main() 
+    plt.show()    
